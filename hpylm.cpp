2e6e71d266bdaa505f409721ce8a87fc773bcac0
musyoku <musyoku@users.noreply.github.com>
2016-11-08 00:45:44 +0900

    j

--- hpylm.cpp
+++ hpylm.cpp
@@ -41,7 +41,7 @@ vector<wstring> split(const wstring &s, char delim) {
 }
 
 // スペースで分割する
-Vocab* load_words_in_textfile(string &filename, vector<wstring> &dataset){
+Vocab* load_words_in_textfile(string &filename, vector<vector<id>> &dataset){
 	wifstream ifs(filename.c_str());
 	wstring str;
 	if (ifs.fail()){
@@ -50,23 +50,31 @@ Vocab* load_words_in_textfile(string &filename, vector<wstring> &dataset){
 		return NULL;
 	}
 	Vocab* vocab = new Vocab();
+	id bos_id = vocab->add_string(L"<bos>");
+	id eos_id = vocab->add_string(L"<eos>");
+	id unk_id = vocab->add_string(L"<unk>");
 	while (getline(ifs, str) && !str.empty()){
 		vector<wstring> words = split(str, ' ');
+		vector<id> token_ids = {bos_id};
 		for(auto word: words){
-			wcout << word << endl;
+			if(word.size() == 0){
+				continue;
+			}
+			id token_id = vocab->add_string(word);
+			token_ids.push_back(token_id);
 		}
-		// dataset.push_back(str);
-		// for(int i = 0;i < str.length();i++){
-		// 	vocab->addCharacter(str[i]);
-		// }
+		token_ids.push_back(eos_id);
+		if(token_ids.size() > 0){
+			dataset.push_back(token_ids);
+		}
 	}
-	cout << "loading " << dataset.size() << " lines." << endl;
+	cout << filename << "を読み込みました（" << dataset.size() << "行）" << endl;
 	return vocab;
 }
 
-void show_progress(int step, int total, double &progress){
-	progress = step / (double)(total - 1);
-	int barWidth = 70;
+void show_progress(int step, int total){
+	double progress = step / (double)(total - 1);
+	int barWidth = 30;
 
 	cout << "[";
 	int pos = barWidth * progress;
@@ -77,139 +85,88 @@ void show_progress(int step, int total, double &progress){
 	}
 	cout << "] " << int(progress * 100.0) << " %\r";
 	cout.flush();
-
-	progress += 0.16;
 }
 
-/*
-void train_hpylm(Vocab* vocab, vector<wstring> &dataset, string model_dir){
+void train(Vocab* vocab, vector<vector<id>> &dataset){
+	string hpylm_filename = "hpylm.model";
+	string vocab_filename = "hpylm.vocab";
+	string trainer_filename = "hpylm.trainer";
+
+	vector<int> rand_indices;
+	vector<bool> is_first_addition;
+	for(int i = 0;i < dataset.size();i++){
+		rand_indices.push_back(i);
+		is_first_addition.push_back(true);
+	}
+
 	int ngram = 3;
 	HPYLM* hpylm = new HPYLM(ngram);
-	int num_chars = vocab->numCharacters();
+	int num_chars = vocab->num_tokens();
 	hpylm->set_g0(1.0 / num_chars);
-	vector<id> sentence_char_ids;
 
-	int max_epoch = 500;
-	int train_per_epoch = dataset.size();
 
-	vector<int> rand_perm;
-	for(int i = 0;i < dataset.size();i++){
-		rand_perm.push_back(i);
-	}
-	random_shuffle(rand_perm.begin(), rand_perm.end());
-
-	unordered_map<int, vector<int> > prev_orders;
-
-	// 読み込み
-	hpylm->load(model_dir);
-	vocab->load(model_dir);
-	string filename = model_dir + "trainer";
-	std::ifstream ifs(filename);
+	hpylm->load(hpylm_filename);
+	vocab->load(vocab_filename);
+	std::ifstream ifs(trainer_filename);
 	if(ifs.good()){
 		boost::archive::binary_iarchive iarchive(ifs);
-		iarchive >> prev_orders;
+		iarchive >> is_first_addition;
 	}
 
-	printf("training in progress...\n");
+	int max_epoch = 500;
+	int num_data = dataset.size();
+
+	cout << "HPYLMを学習しています ..." << endl;
 	for(int epoch = 1;epoch <= max_epoch;epoch++){
+		printf("Epoch %d / %d", epoch, max_epoch);
 		auto start_time = chrono::system_clock::now();
-		double progress = 0.0;
-		random_shuffle(rand_perm.begin(), rand_perm.end());
+		random_shuffle(rand_indices.begin(), rand_indices.end());
 
-		for(int step = 0;step < train_per_epoch;step++){
+		for(int step = 0;step < num_data;step++){
+			show_progress(step, num_data);
+			int data_index = rand_indices[step];
+			vector<id> token_ids = dataset[data_index];
 
-			show_progress(step, train_per_epoch, progress);
-
-			int data_index = rand_perm[step];
-
-			wstring sentence = dataset[data_index];
-			if(sentence.length() == 0){
-				continue;
-			}
-			sentence_char_ids.clear();
-			sentence_char_ids.push_back(vocab->bosId());
-			for(int i = 0;i < sentence.length();i++){
-				int id = vocab->char2id(sentence[i]);
-				sentence_char_ids.push_back(id);
-			}
-			sentence_char_ids.push_back(vocab->eosId());
-
-			if(prev_orders.find(data_index) == prev_orders.end()){
-				 vector<int> prev_order(sentence_char_ids.size(), -1);
-				 prev_orders[data_index] = prev_order;
-			}else{
-				vector<int> &prev_order = prev_orders[data_index];
-				if(prev_order.size() != sentence_char_ids.size()){
-					printf("\x1b[41;97m");
-					printf("WARNING");
-					printf("\x1b[49;39m");
-					printf(" prev_order missmatch.\n");
-				}else{
-					for(int c_t_i = 0;c_t_i < sentence_char_ids.size();c_t_i++){
-						int n_t = prev_order[c_t_i];
-						hpylm->remove(sentence_char_ids, c_t_i, n_t);
-					}				
+			for(int token_t_index = ngram - 1;token_t_index < token_ids.size();token_t_index++){
+				if(is_first_addition[data_index] == false){
+					hpylm->remove_customer_at_timestep(token_ids, token_t_index);
 				}
+				hpylm->add_customer_at_timestep(token_ids, token_t_index);
 			}
-
-			vector<int> &prev_order = prev_orders[data_index];
-			if(prev_order.size() != sentence_char_ids.size()){
-				printf("\x1b[41;97m");
-				printf("WARNING");
-				printf("\x1b[49;39m");
-				printf(" prev_order missmatch.\n");
-			}else{
-				for(int c_t_i = 0;c_t_i < sentence_char_ids.size();c_t_i++){
-					int n_t = hpylm->sampleOrder(sentence_char_ids, c_t_i);
-					hpylm->add(sentence_char_ids, c_t_i, n_t);
-					prev_order[c_t_i] = n_t;
-				}
-			}
+			is_first_addition[data_index] = false;
 		}
 
-		hpylm->sampleHyperParams();
+		hpylm->sample_hyperparams();
 
 		auto end_time = chrono::system_clock::now();
-		auto dur = end_time - start_time;
-		auto msec = chrono::duration_cast<chrono::milliseconds>(dur).count();
+		auto duration = end_time - start_time;
+		auto msec = chrono::duration_cast<chrono::milliseconds>(duration).count();
 
 		// パープレキシティ
-		double vpylm_ppl = 0;
-		for(int i = 0;i < train_per_epoch;i++){
-			wstring sentence = dataset[rand_perm[i]];
-			if(sentence.length() == 0){
-				continue;
-			}
-			sentence_char_ids.clear();
-			sentence_char_ids.push_back(vocab->bosId());
-			for(int i = 0;i < sentence.length();i++){
-				int id = vocab->char2id(sentence[i]);
-				sentence_char_ids.push_back(id);
-			}
-			sentence_char_ids.push_back(vocab->eosId());
-			double log_p = vpylm->log_Pw(sentence_char_ids) / (double)sentence_char_ids.size();
-			vpylm_ppl += log_p;
+		double ppl = 0;
+		for(int step = 0;step < num_data;step++){
+			vector<id> token_ids = dataset[step];
+			double log_p = hpylm->log2_Pw(token_ids) / token_ids.size();
+			ppl += log_p;
 		}
-		vpylm_ppl = exp(-vpylm_ppl / (double)train_per_epoch);
-
-		cout << endl << "[epoch " << epoch << "] " <<  (double)train_per_epoch / msec * 1000.0 << " sentences / sec; perplexity " << vpylm_ppl << endl;
-
+		ppl = exp(-ppl / num_data);
+		printf("Epoch %d / %d - %.1f sentences / sec - %.3f ppl", epoch, max_epoch, (double)num_data / msec * 1000.0, ppl);
 		if(epoch % 100 == 0){
-			vpylm->save(model_dir);
-			vocab->save(model_dir);
+			hpylm->save(hpylm_filename);
+			vocab->save(vocab_filename);
 		}
 	}
 
-	vpylm->save(model_dir);
-	vocab->save(model_dir);
-	std::ofstream ofs(filename);
+	hpylm->save(hpylm_filename);
+	vocab->save(vocab_filename);
+	std::ofstream ofs(trainer_filename);
 	boost::archive::binary_oarchive oarchive(ofs);
-	oarchive << prev_orders;
+	oarchive << is_first_addition;
 
 	// <!-- デバッグ用
 	// 客を全て削除した時に客数が本当に0になるかを確認する場合
-	// for(int step = 0;step < train_per_epoch;step++){
-	// 	int data_index = rand_perm[step];
+	// for(int step = 0;step < num_data;step++){
+	// 	int data_index = rand_indices[step];
 
 	// 	wstring sentence = dataset[data_index];
 	// 	if(sentence.length() == 0){
@@ -233,12 +190,12 @@ void train_hpylm(Vocab* vocab, vector<wstring> &dataset, string model_dir){
 	// 		}else{
 	// 			for(int c_t_i = 0;c_t_i < sentence_char_ids.size();c_t_i++){
 	// 				int n_t = prev_order[c_t_i];
-	// 				bool success = vpylm->remove(sentence_char_ids, c_t_i, n_t);
+	// 				bool success = hpylm->remove(sentence_char_ids, c_t_i, n_t);
 	// 				if(success == false){
 	// 					printf("\x1b[41;97m");
 	// 					printf("WARNING");
 	// 					printf("\x1b[49;39m");
-	// 					printf(" Failed to remove a customer from VPYLM.\n");
+	// 					printf(" Failed to remove a customer from hPYLM.\n");
 	// 				}
 	// 			}
 	// 		}
@@ -246,14 +203,13 @@ void train_hpylm(Vocab* vocab, vector<wstring> &dataset, string model_dir){
 	// }
 	//  -->
 
-	cout << vpylm->maxDepth() << endl;
-	cout << vpylm->numChildNodes() << endl;
-	cout << vpylm->numCustomers() << endl;
-	cout << vpylm->_sum_stop_counts() << endl;
-	cout << vpylm->_sum_pass_counts() << endl;
+	cout << hpylm->get_max_depth() << endl;
+	cout << hpylm->get_num_nodes() << endl;
+	cout << hpylm->get_num_customers() << endl;
+	cout << hpylm->get_sum_stop_counts() << endl;
+	cout << hpylm->get_sum_pass_counts() << endl;
 }
 
-*/
 int main(int argc, char *argv[]){
 	// 日本語周り
 	setlocale(LC_CTYPE, "ja_JP.UTF-8");
@@ -263,40 +219,28 @@ int main(int argc, char *argv[]){
 	locale ctype_default(locale::classic(), default_loc, locale::ctype); //※
 	wcout.imbue(ctype_default);
 	wcin.imbue(ctype_default);
-	vector<wstring> dataset;
-
-	// [arguments]
-	// --textdir ****
-	// 訓練データtrain.txtの入っているフォルダを指定
 
 	string text_filename;
-	string model_filename;
 	cout << "num args = " << argc << endl;
 	if(argc % 2 != 1){
-		cout << "invalid command line arguments." << endl;
-		return -1;
+		c_printf("[R]%s", "エラー");
+		c_printf("[n] %s\n", "テキストファイルを指定してください. -t example.txt");
+		exit(1);
 	}else{
 		for (int i = 0; i < argc; i++) {
 			cout << i << "-th args = " << argv[i] << endl; 
 			if (string(argv[i]) == "-t" || string(argv[i]) == "--text") {
 				if(i + 1 >= argc){
-					cout << "invalid command line arguments." << endl;
-					return -1;
+					c_printf("[R]%s", "エラー");
+					cout << "不正なコマンドライン引数です." << string(argv[i]) << endl;
+					exit(1);
 				}
 				text_filename = string(argv[i + 1]);
 			}
-			else if (string(argv[i]) == "-m" || string(argv[i]) == "--model") {
-				if(i + 1 >= argc){
-					cout << "invalid command line arguments." << endl;
-					return -1;
-				}
-				model_filename = string(argv[i + 1]);
-			}
 		}
 	}
-
+	vector<vector<id>> dataset;
 	Vocab* vocab = load_words_in_textfile(text_filename, dataset);
-
-	// train_vpylm(vocab, dataset, model_filename);
+	train(vocab, dataset);
 	return 0;
 }
--- vpylm/vocab.h
+++ vpylm/vocab.h
@@ -1,6 +1,6 @@
 #ifndef _vocab_
 #define _vocab_
-#include <stdlib.h>
+#include <cstdlib>
 #include <unordered_map>
 #include <map>
 #include <string>
@@ -13,6 +13,7 @@
 #include <boost/serialization/unordered_map.hpp>
 #include <boost/serialization/map.hpp>
 #include <boost/serialization/vector.hpp>
+#include "c_printf.h"
 
 using namespace std;
 using id = unsigned long long;
@@ -20,134 +21,64 @@ using id = unsigned long long;
 class Vocab{
 private:
 	// wstring -> characters -> word
-	unordered_map<wstring, id> _token_id_by_wstring;
-	unordered_map<id, wstring> _wstring_by_token_id;
-	unordered_map<id, vector<id> > _word_dict;
-	map<vector<id>, id> _word_dict_inv;
-	vector<id> _reusable_ids;
-	vector<id> _null_word;
+	unordered_map<wstring, id> _token_id_by_string;
+	unordered_map<id, wstring> _string_by_token_id;
+	vector<id> _reusable_token_ids;
 	id _auto_increment;
-	id _unk_id;
-	id _bos_id;
-	id _eos_id;
 	
 	friend class boost::serialization::access;
 	template <class Archive>
 	void serialize(Archive& archive, unsigned int version)
 	{
 		static_cast<void>(version); // No use
-		archive & _token_id_by_wstring;
-		archive & _wstring_by_token_id;
-		archive & _word_dict;
-		archive & _word_dict_inv;
-		archive & _reusable_ids;
+		archive & _token_id_by_string;
+		archive & _string_by_token_id;
+		archive & _reusable_token_ids;
 		archive & _auto_increment;
 	}
-
 public:
 	Vocab(){
-		_bos_id = 0;
-		_eos_id = 1;
-		_wstring_by_token_id[_bos_id] = L'^';
-		_wstring_by_token_id[_eos_id] = L'$';
-		_auto_increment = _eos_id;
-		_null_word = {0};
+		_auto_increment = 0;
 	}
-	id generateId(){
-		if(_reusable_ids.size() == 0){
+	id generate_token_id(){
+		if(_reusable_token_ids.size() == 0){
 			_auto_increment++;
 			return _auto_increment;
 		}
-		id used_id = _reusable_ids.back();
-		_reusable_ids.pop_back();
-		return used_id;
+		id token_id = _reusable_token_ids.back();
+		_reusable_token_ids.pop_back();
+		return token_id;
 	}
-	void addCharacter(wstring ch){
-		if(_token_id_by_wstring.find(ch) == _token_id_by_wstring.end()){
-			id gen_id = generateId();
-			_token_id_by_wstring[ch] = gen_id;
-			_wstring_by_token_id[gen_id] = ch;
-			if(_word_dict.find(gen_id) == _word_dict.end()){
-				_word_dict[gen_id] = {gen_id};
-			}
-		}
-	}
-	id char2id(wstring ch){
-		auto itr = _token_id_by_wstring.find(ch);
-		if(itr == _token_id_by_wstring.end()){
-			id gen_id = generateId();
-			_token_id_by_wstring[ch] = gen_id;
-			_wstring_by_token_id[gen_id] = ch;
-			return gen_id;
+	id add_string(wstring str){
+		auto itr = _token_id_by_string.find(str);
+		if(itr == _token_id_by_string.end()){
+			id token_id = generate_token_id();
+			_token_id_by_string[str] = token_id;
+			_string_by_token_id[token_id] = str;
+			return token_id;
 		}
 		return itr->second;
 	}
-	wstring id2char(id id){
-		if(_wstring_by_token_id.find(id) == _wstring_by_token_id.end()){
-			return L' ';
-		}
-		return _wstring_by_token_id[id];
-	}
-	id characters2word(vector<id> &char_ids){
-		if(char_ids.size() == 1){
-			return char_ids[0];
-		}
-		auto itr = _word_dict_inv.find(char_ids);
-		if(itr == _word_dict_inv.end()){
-			id gen_id = generateId();
-			_word_dict_inv[char_ids] = gen_id;
-			// cout << "word vocab generated " << gen_id << endl;
-			_word_dict[gen_id] = char_ids;
-			return gen_id;
+	id string_to_token_id(wstring str){
+		auto itr = _token_id_by_string.find(str);
+		if(itr == _token_id_by_string.end()){
+			id token_id = generate_token_id();
+			_token_id_by_string[str] = token_id;
+			_string_by_token_id[token_id] = str;
+			return token_id;
 		}
 		return itr->second;
 	}
-	vector<id> &word2characters(id word_id){
-		auto itr = _word_dict.find(word_id); 
-		if (itr == _word_dict.end()) {
-			return _null_word;
+	wstring token_id_to_string(id token_id){
+		if(_string_by_token_id.find(token_id) == _string_by_token_id.end()){
+			c_printf("[R]%s", "エラー");
+			c_printf("[n]%s\n", " 存在しないトークンIDです. _string_by_token_id.find(token_id) == _string_by_token_id.end()");
+			exit(1);
 		}
-		return itr->second;
+		return _string_by_token_id[token_id];
 	}
-	wstring word2string(id word_id){
-		if(_word_dict.find(word_id) == _word_dict.end()){
-			if(_wstring_by_token_id.find(word_id) == _wstring_by_token_id.end()){
-				return L"";
-			}
-			wstring str;
-			str += id2char(word_id);
-			return str;
-		}
-		vector<id> &char_ids = _word_dict[word_id];
-		return characters2string(char_ids);
-	}
-	wstring characters2string(vector<id> &char_ids){
-		wstring str;
-		for(int i = 0;i < char_ids.size();i++){
-			str += id2char(char_ids[i]);
-		}
-		return str;
-	}
-	void string2characters(wstring str, vector<id> &ids){
-		ids.clear();
-		for(int i = 0;i < str.length();i++){
-			ids.push_back(char2id(str[i]));
-		}
-	}
-	int numWords(){
-		return _word_dict.size();
-	}
-	int numCharacters(){
-		return _token_id_by_wstring.size();
-	}
-	id eosId(){
-		return _eos_id;
-	}
-	id bosId(){
-		return _bos_id;
-	}
-	id autoIncrement(){
-		return _auto_increment;
+	int num_tokens(){
+		return _string_by_token_id.size();
 	}
 	void save(string dir = "model/"){
 		string filename = dir + "vocab";
@@ -155,10 +86,8 @@ public:
 		boost::archive::binary_oarchive oarchive(ofs);
 		oarchive << static_cast<const Vocab&>(*this);
 		cout << "saved to " << filename << endl;
-		cout << "	num_words: " << numWords() << endl;
-		cout << "	num_characters: " << numCharacters() << endl;
-		cout << "	auto_increment: " << _auto_increment << endl;
-		cout << "	num_reusable_ids: " << _reusable_ids.size() << endl;
+		cout << "	num_tokens: " << num_tokens() << endl;
+		cout << "	num_reusable_ids: " << _reusable_token_ids.size() << endl;
 	}
 
 	void load(string dir = "model/"){
@@ -168,40 +97,26 @@ public:
 			cout << "loading " << filename << endl;
 			boost::archive::binary_iarchive iarchive(ifs);
 			iarchive >> *this;
-			cout << "	num_words: " << numWords() << endl;
-			cout << "	num_characters: " << numCharacters() << endl;
-			cout << "	auto_increment: " << _auto_increment << endl;
-			cout << "	num_reusable_ids: " << _reusable_ids.size() << endl;
+			cout << "	num_tokens: " << num_tokens() << endl;
+			cout << "	num_reusable_ids: " << _reusable_token_ids.size() << endl;
 		}
 	}
 	void dump(){
-		for(auto kv : _wstring_by_token_id) {
-			int key = kv.first;
-			wstring charactor = kv.second;
-			wcout << key << ": " << charactor << endl;
+		for(auto elem : _string_by_token_id) {
+			wcout << elem.first << ": " << elem.second << endl;
 		} 
 	}
-	void clean(unordered_map<id, bool> &active_keys){
-		unordered_map<id, vector<id> > new_word_dict;
-		map<vector<id>, id> new_word_dict_inv;
-		
-		for(auto elem: _word_dict){
-			id word_id = elem.first;
-			if(active_keys.find(word_id) == active_keys.end()){
-				if(_wstring_by_token_id.find(word_id) == _wstring_by_token_id.end()){
-					_reusable_ids.push_back(word_id);
+	void remove_unused_token_ids(unordered_map<id, bool> &active_token_ids){
+		for(auto elem: _string_by_token_id){
+			id token_id = elem.first;
+			if(active_token_ids.find(token_id) == active_token_ids.end()){
+				if(_string_by_token_id.find(token_id) == _string_by_token_id.end()){
+					_reusable_token_ids.push_back(token_id);
 				}
 			}else{
-				vector<id> dict = _word_dict[word_id];
-				new_word_dict[word_id] = dict;
-				new_word_dict_inv[dict] = word_id;
+
 			}
 		}
-
-		_word_dict.clear();
-		_word_dict_inv.clear();
-		_word_dict = new_word_dict;
-		_word_dict_inv = new_word_dict_inv;
 	}
 };
 
